ab16dbf1e7d0429709251cde4eebc1424cc907d1
CAussant <aussant.come@gmail.com>
2025-12-10 10:36:04 +0100

    first commit

--- src/main/java/bts/sio/azurimmo/model/Appartement.java
+++ src/main/java/bts/sio/azurimmo/model/Appartement.java
@@ -1,9 +1,36 @@
 package bts.sio.azurimmo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "appartement")
 public class Appartement {
 	
-	private integer id;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	 
+	@Column(name="numero")
+	private Long numero;
+
+	@Column(name="surface")
+	private Float surface;
+	 
+	@Column(name="nbPieces")
+	private Float nbPieces;
+	 
+	@Column(name="description")
+	private String description;
 	
 	@ManyToOne
 	@JoinColumn(name = "batiment_id")
--- src/main/java/bts/sio/azurimmo/repository/AppartementRepository.java
+++ src/main/java/bts/sio/azurimmo/repository/AppartementRepository.java
@@ -1,5 +1,9 @@
 package bts.sio.azurimmo.repository;
 
-public class AppartementRepository {
+import org.springframework.stereotype.Repository;
+import bts.sio.azurimmo.model.Appartement;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
+public interface AppartementRepository extends JpaRepository<Appartement, Long> {
 }
